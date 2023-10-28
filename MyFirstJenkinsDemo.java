28fa7869a40ca1d3554bb76db2175227185158bb
Mohan <mohan.infyjz@gmail.com>
2023-10-28 09:46:44 +0530

    This is a JavaProject

---
+++ MyFirstJenkinsDemo.java
@@ -0,0 +1,9 @@
+import java.io.*
+import java.lang.*
+public class MyFirstJenkinsDemo{
+
+public static void main(String args[]){
+string a="Jesus is our Saviour";
+system.out.Println(" Word of God ::"+a);
+}
+}
